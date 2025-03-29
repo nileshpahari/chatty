016de3e8d289af56b0a7dbbc5b8dc719a595e09a
Nilesh Pahari <nileshpahari@protonmail.com>
2025-03-29 23:38:43 +0530

    init backend

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,6 @@
+const login = async function login() {};
+const signup = async function signup() {};
+const logout = async function logout() {};
+
+
+export {login, signup, logout}
---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import app from "./app"
+import dbConnect from "./db/index"
+
+dotenv.config();
+
+const port = process.env.PORT || 5001;
+
+dbConnect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log("http server started at port ", port);
+    });
+  })
+  .catch((err: any) => {
+    console.log("failed to connect to db, exiting...\n", err);
+  });
