041f1163edc42a363a49758d7b584e8f690535c2
Nilesh Pahari <nileshpahari@protonmail.com>
2025-04-16 01:27:20 +0530

    add router and theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router-dom";
+import { useThemeStore } from "./store/useThemeStore";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const {authUser, getAuthUser, isCheckingAuth} = useAuthStore()
+  const { theme } = useThemeStore();
+  console.log(theme);
+  useEffect(()=>{
+    getAuthUser()
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div datatype="theme" data-theme={theme}>
+        <Outlet />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,62 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import Protected from "./components/Protected.tsx";
+import Login from "./pages/Login.tsx";
+import Settings from "./pages/Settings.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Profile from "./pages/Profile.tsx";
+import Chat from "./pages/Chat.tsx";
+import Signup from "./pages/Signup.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "login",
+        element: (
+          <Protected authRequired={false}>
+            <Login />
+          </Protected>
+        ),
+      },
+      {
+        path: "signup",
+        element: (
+          <Protected authRequired={false}>
+            <Signup />
+          </Protected>
+        ),
+      },
+      {
+        path: "settings",
+        element: <Settings />,
+      },
+      {
+        path: "profile",
+        element: (
+          <Protected>
+            <Profile />
+          </Protected>
+        ),
+      },
+      {
+        path: "chat",
+        element: (
+          <Protected>
+            <Chat />
+          </Protected>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
